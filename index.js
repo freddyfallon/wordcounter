9603c22fe1fd20644f6c19a2c244be6b22cbf916
Freddy Fallon <freddyfallon@gmail.com>
2017-09-21 19:33:02 +0100

    Add a message at the top of the app when it runs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import readFile from 'fs-readfile-promise';
 readFile('./assets/TheRailWayChildren.txt', 'utf-8')
 .then((text) => {
 	const counter = new WordCounter(text);
+	console.log("Here are all of the words!")
 	counter.run();
 });
 
