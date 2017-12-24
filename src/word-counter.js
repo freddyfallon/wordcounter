463949400dd838af2ec114e3a4ee7ff327a87f22
Freddy Fallon <freddyfallon@gmail.com>
2017-12-24 12:05:59 +0000

    🎨 Clean structure of word-counter.js

--- src/word-counter.js
+++ src/word-counter.js
@@ -1,62 +1,61 @@
 import _ from 'lodash';
 
 export default class WordCounter {
-	constructor(text) {
-		this.text = text;
-		this.wordCount = {};
-		this.primeNumbers = [];
-	}
+  constructor(text) {
+    this.text = text;
+    this.wordCount = {};
+    this.primeNumbers = [];
+  }
 
-	makeLowerCase() {
-		this.text = this.text.toLowerCase();
-	}
+  makeLowerCase() {
+    this.text = this.text.toLowerCase();
+  }
 
-	purifyText() {
-		this.text = this.text.replace(/[^0-9a-z\ ]/gi, ' ').replace(/\s+/g, " ")
-	}
+  purifyText() {
+    this.text = this.text.replace(/[^0-9a-z\ ]/gi, ' ').replace(/\s+/g, ' ');
+  }
 
-	splitWords() {
-		this.text = this.text.split(" ");
-	}
+  splitWords() {
+    this.text = this.text.split(' ');
+  }
 
-	countWords() {
-		this.text.forEach((word) => {
-			this.wordCount.hasOwnProperty(word) ? this.wordCount[word] ++ : this.wordCount[word] = 1;
-		})
+  countWords() {
+    this.text.forEach((word) => {
+      this.wordCount.hasOwnProperty(word) ? this.wordCount[word]++ : this.wordCount[word] = 1;
+    });
 
-		return this.wordCount;
-	}
+    return this.wordCount;
+  }
 
-	getPrimes() {
-		_.forIn(this.wordCount, (value, key) => {
-			let prime = true;
-			for(let i = 2; i < value; i++) {
-				if (value % i === 0) {
-					prime = false;
-				}
-			}
-			if (value === 1) {prime = false};
-			if (prime === true) {
-				this.primeNumbers.push(this.wordCount[key]);
-			}
-		})
-	}
+  getPrimes() {
+    _.forIn(this.wordCount, (value, key) => {
+      let prime = true;
+      for (let i = 2; i < value; i++) {
+        if (value % i === 0) {
+          prime = false;
+        }
+      }
+      if (value === 1) { prime = false; }
+      if (prime === true) {
+        this.primeNumbers.push(this.wordCount[key]);
+      }
+    });
+  }
 
-	showWords() {
-		_.forIn(this.wordCount, (value, key) => {
-			if (this.primeNumbers.includes(value)) {
-				console.log(key + ": " + value + " - prime number");
-			} else console.log(key + ": " + value);
-		})
-	}
+  showWords() {
+    _.forIn(this.wordCount, (value, key) => {
+      if (this.primeNumbers.includes(value)) {
+        console.log(`${key}: ${value} - prime number`);
+      } else console.log(`${key}: ${value}`);
+    });
+  }
 
-	run() {
-		this.makeLowerCase();
-		this.purifyText();
-		this.splitWords();
-		this.countWords();
-		this.getPrimes();
-		this.showWords();
-	}
-
-};
+  run() {
+    this.makeLowerCase();
+    this.purifyText();
+    this.splitWords();
+    this.countWords();
+    this.getPrimes();
+    this.showWords();
+  }
+}
